05bf4731fbd9da39903459820410ea7a3f3f0f94
Fernando <Fernando@DESKTOP-01G8JTV>
2019-11-10 17:06:46 -0300

    promedio_test frecuenciaPorcentual_test Todoslostest -> agregar las clases a ser testeadas a: @Suite.SuiteClasses({class1.class , class2.class, class3.class, etc...})

---
+++ test/Todoslostest.java
@@ -0,0 +1,14 @@
+package test;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ Calculador_frecuenciaPorcentual_test.class, Calculador_promedio_test.class })
+
+
+public class Todoslostest {
+
+}
