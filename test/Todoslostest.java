16917ad3e921d9356ca211da9ba35faa093ffb03
Fernando <Fernando@DESKTOP-01G8JTV>
2019-11-16 19:46:17 -0300

    Agregue todoas las clases a Todoslostest.java

--- test/Todoslostest.java
+++ test/Todoslostest.java
@@ -6,9 +6,16 @@ import org.junit.runners.Suite;
 
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ Calculador_frecuenciaPorcentual_test.class, Calculador_promedio_test.class })
+@Suite.SuiteClasses({ Calculador_frecuenciaPorcentual_test.class, Calculador_promedio_test.class,
+                      Calculador_resta_test.class, Calculador_suma_test.class,
+                      ConjuntoDatosNoNumericos_getColumna_Test.class,
+                      ConjuntoDatosNumericos_actualizarColumna_Test.class, ConjuntoDatosNumericos_getColumna_test.class,
+                      Parser_obtenerTokens_test.class, ParserArchivo_grabarDatos_test.class,
+                      ParserArchivo_obtenerConjuntoDatos_test.class
+    })
 
 
-public class Todoslostest {
+public class Todoslostest
+{
 
 }
